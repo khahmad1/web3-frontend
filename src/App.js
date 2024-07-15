a8369514ec2f02856d9c09c28c11af7200fcbd72
khahmad1 <ahmad2003kharma@gmail.com>
2024-07-15 11:36:55 +0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router";
+import CustomerLayout from "./layouts/customerLayout";
+import AboutUsPAge from "./pages/AboutUsPage";
+import ContactUsPAge from "./pages/ContactUsPage";
+import { ToastContainer } from "react-toastify";
+import HomePage from "./pages/HomePage";
+import AdminLayout from "./layouts/adminLayout";
+import NotFound from "./pages/notFound";
+import SignIn from "./pages/signInPage";
+import SignUp from "./pages/SignUpPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Routes>
+        {/* <Route path="/dashboard" element={<LoginAdmin />}> */}
+        <Route path="/dashboard" element={<AdminLayout />}>
+          {/* <Route path="dashboard" element={<Dashboard />} />
+            <Route path="admin" element={<AdminTable />} />
+            <Route path="category" element={<CategoryTable />} />
+            <Route path="facility" element={<FacilityTable />} />
+            <Route path="medicine" element={<MedicineTable />} />
+            <Route path="message" element={<MessageTable />} />
+            <Route path="order" element={<OrderTable />} /> */}
+        </Route>
+        {/* </Route> */}
+        <Route path="/" element={<CustomerLayout />}>
+          <Route path="/" element={<HomePage />} />
+
+          <Route path="/contact-us" element={<ContactUsPAge />} />
+          <Route path="/about" element={<AboutUsPAge />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000} // Duration to display each notification in milliseconds (e.g., 3000 = 3 seconds)
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
