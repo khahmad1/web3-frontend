cbfc9928503231d17be0dafe5e57c3dbf0ee9aee
Karim-Chami <karimchamich@gmail.com>
2024-07-18 13:26:41 +0300

    profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,20 +4,47 @@ import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import EmailIcon from '@mui/icons-material/Email';
 import "../style/profile.css";
 import { useState, useEffect } from "react";
-import axios from "axios";
-import { URL_SERVER } from "../constant";
+import LogoutIcon from '@mui/icons-material/Logout';
+import  Button  from "../components/button.jsx";
 export default function Profile(){
+    const [data,setData] = useState({}); 
+    // useEffect(()=>{
+    //     const session = localStorage.getItem("session");        
+    //     setData(
+    //         session.user);
+    // },[]);
+    
     return (
-       <div className="profile">
-            <h2 className="name">Facility name: user<div className="blue-bar"></div></h2>
+        <div className="profile">
+            <h1>Facility Name: {data.user} <div class="blue-bar"></div></h1>
             <div className="behind">
-                <label><HomeIcon class="profile-icon"/> address: lebanon, beirut</label>
-                <label><LocalPhoneIcon class="profile-icon"/>phone: 70550776</label>
-                <label><EmailIcon class="profile-icon"/>Email: karimchamich@gmail.com</label> 
+                <label><HomeIcon className="profile-icon"/>Address: <span className="data">{data.address}</span></label>
+                <label> <EmailIcon className="profile-icon"/>Email: <span className="data">{data.email}</span></label>
+                <label><LocalPhoneIcon className="profile-icon"/>Phone: <span className="data">{data.phone}</span> </label>
             </div>
+            
+            <div className="container">
+            <div className="profile-vertical">
             <div className="profile-picture">
-                <img src={`${URL_SERVER}/storage/logos/KIjtAVGiuGiICaQ63o5zk2k2pc1kNzd2PHFYEETC.png`}/>
+                <img src={data.logo} alt="logo"/>
             </div>
-       </div> 
-    )
+                <h2>Requested Order</h2>
+                <span>{data.orders}</span>
+                <div className="buttons"><Button link="/choose-your-product" text="Get your order now"/>
+                <Button link="/logout" text={<LogoutIcon/>}/>
+                </div>
+            </div>
+            
+                <table> 
+                    <tr>
+                        <th>Facility</th>
+                        <th>Date</th>
+                        <th>Medicine</th>
+                        <th>Status</th>
+                        <th>Price</th>
+                    </tr>
+                </table>
+                </div>
+                </div>
+            )
 }
